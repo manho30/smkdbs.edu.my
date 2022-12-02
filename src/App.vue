cea5d1c072091c55254c04fab874777d75ed67ed
manho30 <manhochan30@gmail.com>
2022-12-02 16:29:51 +0800

    upgrade to vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header-container></header-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import HeaderContainer from './components/header.vue'
+  export default {
+    name: 'App',
+    components: {
+      HeaderContainer
+    }
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="header-container">
+    <a class="p-3 bg-white shadow rounded-lg flex items-center">
+      <div id="logo-sklh">
+        <img src="../assets/logo.png" alt="Lencana SMK DATO BIJAYA SETIA"
+             className="max-w-full h-auto lg:block" height="31.5" width="42">
+      </div>
+      <div id="logo-kpm">
+        <img src="../assets/malaysia.png" alt="Lencana KPM"
+             className="max-w-full h-auto lg:block" height="31.5" width="42">
+      </div>
+      <div id="title" className="ml-2">
+        <h1 className="font-inherit font-extrabold mt-1 text-gray-800 lg:text-3xl md:text-2xl sm:text-xl">
+          SMK DATO BIJAYA SETIA</h1>
+      </div>
+    </a>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'header-container'
+}
+</script>
