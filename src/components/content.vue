a382e5f7895b9bf12720287883e8659e0738ef67
manho30 <manhochan30@gmail.com>
2022-12-02 22:28:12 +0800

    add penguguman element

--- src/components/content.vue
+++ src/components/content.vue
@@ -7,28 +7,34 @@
         <div class="md:w-1/2">
           <h1 class="font-bold text-gray-800 lg:text-3xl md:text-2xl sm:text-xl">
             Event </h1>
-          <p class="text-gray-700 text-sm mb-5">Event will be held in SMK Dato Bijaya Setia </p>
+          <p class="text-gray-700 text-sm mb-5 lg:text-lg md:text-base sm:text-sm">Event will be held in SMK Dato Bijaya
+            Setia </p>
 
           <ol class="relative border-l border-gray-200 dark:border-gray-700">
             <li class="mb-10 ml-4">
-              <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+              <div
+                  class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
               <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">7/12/2022</time>
               <h3 class="text-lg font-semibold text-black-500">Pertandingan Kawad Kaki</h3>
-              <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Pertandingan kawad kaki antara peringkat sekolah.</p>
+              <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Pertandingan kawad kaki antara
+                peringkat sekolah.</p>
             </li>
             <li class="mb-10 ml-4">
-              <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-              <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">7/12/2022 </time>
+              <div
+                  class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+              <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">7/12/2022</time>
               <h3 class="text-lg font-semibold text-black-500">Aktiviti RIMUP</h3>
             </li>
             <li class="mb-10 ml-4">
-              <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+              <div
+                  class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
               <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">1/12/2022</time>
               <h3 class="text-lg font-semibold text-red-600">Pertandingan Bola Sepak Sesi Pagi [TAMAT]</h3>
               <p class="mb-4 text-base font-normal text-red-600">Pertandingan bola sepak antara peringkat sekolah.</p>
             </li>
             <li class="mb-10 ml-4">
-              <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+              <div
+                  class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
               <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">30/11/2022</time>
               <h3 class="text-lg font-semibold text-red-600">Pertandingan Bola Sepak Sesi Petang [TAMAT]</h3>
               <p class="mb-4 text-base font-normal text-red-600">Pertandingan bola sepak antara peringkat sekolah.</p>
@@ -39,10 +45,33 @@
       </div>
     </div>
   </div>
+  <div class="mt-5 mb-5"></div>
+  <div class="bg-gray-100 rounded-md p-4">
+    <div class="flex flex-col md:flex-row ml-4">
+      <div class="md:w-1/2">
+        <h1 class="font-bold text-gray-800 lg:text-3xl md:text-2xl sm:text-xl">
+          Penguguman </h1>
+        <p class="text-gray-700 text-sm mb-5 lg:text-lg md:text-base sm:text-sm"> Penguguman disampaikan oleh pentadbir</p>
+
+        <ol class="relative border-l border-gray-200 dark:border-gray-700">
+          <li class="mb-10 ml-4">
+            <div
+                class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+            <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">23/11/2022</time>
+            <h3 class="text-lg font-semibold text-black-500">Cuti sekolah</h3>
+            <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Cuti sekolah
+              akan diadakan pada 10/12/2022 hingga 3/1/2023.
+              Sepanjang waktu cuti semua pelajar sesi pagi dikendaki
+              datang ke sekolah untuk kelas tambahan.</p>
+          </li>
+        </ol>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: 'content-container'
-  }
+export default {
+  name: 'content-container'
+}
 </script>
